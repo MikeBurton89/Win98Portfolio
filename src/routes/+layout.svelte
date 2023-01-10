9ab275139f9b45bd4ea8041a1d6ca27d674b9bbb
Michele Puricelli <michele.puricelli89@gmail.com>
2023-01-10 19:04:35 +0100

    added f1 function

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+import "98.css"
+</script>
+
+<style>
+    :global(html){
+        background-size: cover;
+        background-color: #c0c0c0;
+        font-size: 10px;
+    }
+</style>
+
+<slot></slot>
