a19a813e93a1af67646b54a140e98f8fd3c38b30
Michele Puricelli <michele.puricelli89@gmail.com>
2023-01-11 14:46:42 +0100

    added routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,20 +9,21 @@
 </script>
 
 <div class="parent">
-	<div class='list-menu'>
+	<div class="list-menu">
 		<ul class="tree-view">
-			<li>Who Am I?</li>
-			<li>What is web development?</li>
+			<li><a href="/">Who Am I?</a> </li>
 			<li>
-                <details>
-                   <summary>Front End</summary> 
-				<ul>
-					<li>React</li>
-					<li>Svelte</li>
-					<li>HTML/CSS/Javascript</li>
-				</ul>
-                </details>
-				
+				<details>
+					<summary><a href="/Projects">Projects</a> </summary>
+					<ul>
+						<li>React</li>
+						<li>Svelte</li>
+						<li>HTML/CSS/Javascript</li>
+						<li>React</li>
+						<li>Svelte</li>
+						<li>HTML/CSS/Javascript</li>
+					</ul>
+				</details>
 			</li>
 			<li>
 				<details open>
@@ -53,46 +54,40 @@
 					</ul>
 				</details>
 			</li>
-			<li>Special Thanks</li>
+			<li><a href="SpecialThanks">Special Thanks</a> </li>
 		</ul>
 	</div>
-    <div class='main-title'>
-        <div class="window">
-            <div class="title-bar">
-                <div class="title-bar-text">An old school portfolio for an old school developer</div>
-                <div class="title-bar-controls">
-                    <button aria-label="Minimize" />
-                    <button aria-label="Maximize" />
-                    <button aria-label="Close" />
-                </div>
-            </div>
-            <div class="window-body">
-                <h1>Welcome to Mike's Portfolio</h1>
-                <div class="status-bar">
-                    <p class="status-bar-field">
-                        Press F1 or click 
-                        <a target="_blank" href="https://github.com/MikeBurton89">Here</a> 
-                        to see my GitHub
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class='footer-area'>
-        <fieldset ></fieldset>
-    </div>
-	
-    
+	<div class="main-title">
+		<div class="window">
+			<div class="title-bar">
+				<div class="title-bar-text">An old school portfolio for an old school developer</div>
+				<div class="title-bar-controls">
+					<button aria-label="Minimize" />
+					<button aria-label="Maximize" />
+					<button aria-label="Close" />
+				</div>
+			</div>
+			<div class="window-body">
+				<h1>Welcome to Mike's Portfolio</h1>
+				<div class="status-bar">
+					<p class="status-bar-field">
+						Press F1 or click
+						<a target="_blank" href="https://github.com/MikeBurton89">Here</a>
+						to see my GitHub
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<fieldset class="footer-area">
+		<div />
+	</fieldset>
+	<div class="page-container">
+		<slot />
+	</div>
 </div>
-<div class="page-container">
-	<slot />
-</div>
-
-
 
 <svelte:window on:keydown|preventDefault={f10Handler} />
-
-
 
 <style>
 	:global(html) {
@@ -105,38 +100,29 @@
 		display: grid;
 		grid-template-columns: repeat(12, 1fr);
 		grid-template-rows: repeat(6, 1fr);
-		grid-column-gap: 0px;
-		grid-row-gap: 0px;
+		grid-column-gap: 2rem;
+		grid-row-gap: 2rem;
 	}
 
 	.main-title {
 		grid-area: 1 / 4 / 3 / 13;
 	}
 	.list-menu {
-		grid-area: 1 / 1 / 5 / 3;
+		grid-area: 1 / 1 / 5 / 4;
 	}
 	.page-container {
 		grid-area: 3 / 4 / 7 / 13;
 	}
 	.footer-area {
-		grid-area: 5 / 1 / 7 / 3;
-        background-color: #dfdfdf;
+		grid-area: 5 / 1 / 7 / 4;
+		background-color: #dfdfdf;
 	}
 
-    .container {
-		display: flex;
-		justify-content: space-between;
-	}
 	.window {
 		place-items: center;
-		height: 21vh;
 	}
 
 	.title-bar-text {
 		font-size: 1rem;
-	}
-	.tree-view {
-		width: 100%;
-		height: 50%;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-	
+	let dateOfBirth: Date = new Date('06/28/1989');
+	let ageDiff: Date = new Date(Date.now() - dateOfBirth.getTime());
+	let year: number = ageDiff.getFullYear();
+	let age: number = year - 1970;
 </script>
 
+<div class="window">
+	<div class="title-bar">
+		<div class="title-bar-text">Who am I?</div>
+		<div class="title-bar-controls">
+			<button aria-label="Minimize" />
+			<button aria-label="Maximize" />
+			<button aria-label="Close" />
+		</div>
+	</div>
+	<div class="window-body">
+		<p>I'm a {age} years old web developer from Italy.</p>
+	</div>
+</div>
 
-<div class='window'></div>
 <style>
-
 </style>
